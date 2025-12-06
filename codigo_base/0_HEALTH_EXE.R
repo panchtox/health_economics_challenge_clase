5e0fc0ae9dd57ebf1668edb4827aa05931486391
panchtox <franaf@gmail.com>
2025-12-06 09:18:22 +0000

    Cambiar a CONFIG_basico.yml

--- codigo_base/0_HEALTH_EXE.R
+++ codigo_base/0_HEALTH_EXE.R
@@ -11,7 +11,7 @@ carpeta_base <- getwd()  # En Posit Cloud será /cloud/project
 setwd(carpeta_base)
 objetos_trans_script <- c("experiment_dir","experiment_lead_dir","carpeta_base","objetos_trans_script")
 
-PARAMS <- yaml.load_file("./codigo_base/CONFIG_minimo_dsreducido.yml")
+PARAMS <- yaml.load_file("./codigo_base/CONFIG_basico.yml")
 
 # Carpetas de experimento
 experiment_dir <- paste(PARAMS$experiment$experiment_label,PARAMS$experiment$experiment_code,sep = "_")
