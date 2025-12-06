4351895c0f4591439de4fdac517b78a77ea77b8e
panchtox <franaf@gmail.com>
2025-12-06 09:46:03 +0000

    corrida incial

--- codigo_base/0_HEALTH_EXE.R
+++ codigo_base/0_HEALTH_EXE.R
@@ -13,7 +13,6 @@ objetos_trans_script <- c("experiment_dir","experiment_lead_dir","carpeta_base",
 
 PARAMS <- yaml.load_file("./codigo_base/CONFIG_basico.yml")
 PARAMS$environment$base_dir <- carpeta_base  # Usar ruta absoluta
-
 # Carpetas de experimento
 experiment_dir <- paste(PARAMS$experiment$experiment_label,PARAMS$experiment$experiment_code,sep = "_")
 experiment_lead_dir <- paste(PARAMS$experiment$experiment_label,PARAMS$experiment$experiment_code,paste0("f",PARAMS$feature_engineering$const$orden_lead),sep = "_")
